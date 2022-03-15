aa51fe81ad14e5cf59986dd5721c4d851ac706ac
dandavisjs <dandavisjs@protonmail.com>
2022-03-15 02:02:24 -0700

    Minor style changes: color

--- components/quiz/quiz.js
+++ components/quiz/quiz.js
@@ -86,7 +86,7 @@ const Quiz = ({ questions, title }) => {
                                 <div className="questions ">
                                     {questions[n].answers.map((answer, i) => {
                                         return (
-                                            <div key={i} onClick={() => !selected && handleCheck(i + 1)} style={questions[n].answer === ++i ? { backgroundColor: 'green', color: 'white' } : null} className="noselect">
+                                            <div key={i} onClick={() => !selected && handleCheck(i + 1)} style={questions[n].answer === ++i ? { backgroundColor: '#F7F7FC', color: 'white' } : null} className="noselect">
                                                 <span >
                                                     {translate ? answer.rus : answer.eng}
                                                 </span>
