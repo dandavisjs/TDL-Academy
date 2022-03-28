e157ed246e297362e67a2e128ff321dd04b6a89d
dandavisjs <dandavisjs@protonmail.com>
2022-03-28 09:30:37 -0700

    FB Pixel added: env var name

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -6,6 +6,7 @@ import Footer from './footer'
 export default function Home(props) {
     return (
         <div className={styles.container}>
+            <Pixel name='FACEBOOK_PIXEL_1' />
             <Navbar />
             <main className={styles.main}>
                 {props.children}
